23d09bf19b3e527600a2a37191f8f092bad4eb5e
Andikas <andpuji27@gmail.com>
2023-09-12 11:03:18 +0800

    Initial Commit

---
+++ Popina/PopinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopinaApp.swift
+//  Popina
+//
+//  Created by user236913 on 9/12/23.
+//
+
+import SwiftUI
+
+@main
+struct PopinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
